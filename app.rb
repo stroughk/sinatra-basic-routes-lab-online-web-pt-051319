0fa7d534306d6dab5ad11127adabaf3af55ddc74
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-06 00:17:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kateryna"
+  end
+
+  get '/hometown' do
+  "My hometown is Little Elm"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Thunder"
+  end
+
 end
